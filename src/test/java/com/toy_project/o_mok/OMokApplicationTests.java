cbe0f708632cbe34f7551bf326112d7e970be383
song-eunhye <dmsgp8292@gmail.com>
2023-08-07 23:43:49 +0900

    first commit

---
+++ src/test/java/com/toy_project/o_mok/OMokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy_project.o_mok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OMokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
